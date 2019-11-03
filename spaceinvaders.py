390c5711c0e968fa0ac78e92ad5eef31d8fdf2fc
Jack Militante <jackmilitante10@gmail.com>
2019-11-02 22:34:18 -0500

    first commit

---
+++ spaceinvaders.py
@@ -0,0 +1,37 @@
+import turtle
+import os
+
+#Set up the screen
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Space Invaders")
+
+#Draw border
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for i in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+#create the player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+
+
+
+
+
+
+delay = raw_input("press enter to finish")
